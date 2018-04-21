124a5ae095f18b544b1c92bd001fee67e39781a8
Michal Miškerník <michal@miskernik.sk>
2018-04-21 17:12:04 +0100

    Fixed hello script

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 import logging
+import random
 from amusementpark.messages import LocalMessage
-from network_setup import create_network
+from amusementpark.visitor_repository import Repository, State
+from helpers import create_node_infos, create_gate_nodes
 
 logging.basicConfig(level=logging.INFO)
 
-nodes, running = create_network({
+network_map = {
     'a': set('bj'),
     'b': set('agc'),
     'c': set('bde'),
@@ -15,8 +17,14 @@ nodes, running = create_network({
     'h': set('gi'),
     'i': set('fh'),
     'j': set('ag'),
-})
+}
 
-for _, broker, _ in running.values():
+repository = Repository('repository.json')
+repository.write_state(State(capacity=3, visitors=[]))
+
+gate_node_infos = create_node_infos(network_map.keys())
+gate_nodes = create_gate_nodes(gate_node_infos, network_map, repository)
+
+for _, broker, _ in gate_nodes.values():
     message = LocalMessage('say_hello')
     broker.add_incoming_message(message)
