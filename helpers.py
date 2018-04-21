606e2d731d6703e7f032981d9687dab14325ca82
Michal Miškerník <michal@miskernik.sk>
2018-04-21 10:44:19 +0100

    Use random free port

--- helpers.py
+++ helpers.py
@@ -1,20 +1,23 @@
 import random
 import itertools
+import socket
 from amusementpark.gate_node import GateNode
 from amusementpark.visitor_node import VisitorNode
 from amusementpark.broker import Broker
 from amusementpark.network import Network
 from amusementpark.node_info import NodeInfo
 
+def get_free_port():
+    server = socket.socket()
+    server.bind(('localhost', 0))
+    _, port = server.getsockname()
+    server.close()
+    return port
+
 def create_node_infos(node_names):
     return {
-        name: NodeInfo(id=node_id, address=('localhost', port), capacity=random.randint(1, 10))
-
-        for name, node_id, port in zip(
-            node_names,
-            random.sample(range(100, 1000), k=len(node_names)),
-            range(8001, 9000)
-        )
+        name: NodeInfo(id=node_id, address=('localhost', get_free_port()), capacity=random.randint(1, 10))
+        for name, node_id in zip(node_names, random.sample(range(100, 1000), k=len(node_names)))
     }
 
 def create_gate_nodes(node_infos, node_neighbours, repository):
@@ -36,20 +39,19 @@ def create_gate_nodes(node_infos, node_neighbours, repository):
     return nodes
 
 visitor_node_id = 2000
-visitor_node_port = 9000
 
 def create_visitor_node():
     global visitor_node_id, visitor_node_port
 
-    node_info = NodeInfo(visitor_node_id, ('localhost', visitor_node_port), 1)
+    port = get_free_port()
+    node_info = NodeInfo(visitor_node_id, ('localhost', port), 1)
     visitor_node = VisitorNode(node_info)
     broker = Broker()
-    network = Network(visitor_node_port, broker)
+    network = Network(port, broker)
 
     broker.run(visitor_node)
     network.run()
 
     visitor_node_id += 1
-    visitor_node_port += 1
 
     return node_info, visitor_node, broker, network
